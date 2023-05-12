a27bb28b28802e4c95d0cbfdbef0b5a6bdb62924
Maite <mayte_mdp@hotmail.com>
2023-05-12 20:25:20 -0300

    [ADD] Validacion de password

--- equipo-12/pages/registro/index.tsx
+++ equipo-12/pages/registro/index.tsx
@@ -3,22 +3,45 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useForm } from 'react-hook-form';
+import axios from "axios";
 import * as yup from "yup"
+import error from 'next/error';
+import { useState } from 'react';
+
 
 const schema = yup.object({
-  email: yup.string().required("Se requiere de un correo electronico").email("Se solicita un correo electronico valido"),
+  password: yup
+  .string()
+  .required("La contraseña es requerida")
+  .min(6, 'La contraseña debe tener al menos 6 caracteres')
+  .max(20, 'La contraseña no puede tener más de 20 caracteres')
+  .matches(/^(?=.*[!@#$%^&*(),.?":{}|<>])(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z]).{6,20}$/, 'La contraseña debe contener al menos 1 carácter especial, una mayúscula y un número'),
 }).required();
 type FormData = yup.InferType<typeof schema>;
 
 const Register = () => {
+  const [error, setError] = useState(false)
   const router = useRouter();
   const { register, handleSubmit, formState:{ errors } } = useForm<FormData>({
     resolver: yupResolver(schema)
   });
-  const onSubmit = ( data : FormData ) => router.push({ 
-    pathname: "/registroExitoso",
-    query: { email : data.email }
-   });
+  const onSubmit = async (data: FormData) => {
+    try {
+    await axios
+      .post("http://localhost:3000/registro", {
+        password: data.password,
+      })
+      .then(function (response) {
+        console.log(response);
+        setError(false)
+        router.push("/");
+      })
+    }catch (error){
+        console.log(error);
+        setError(true)
+    }
+  };
+
     return (
       <main >
         <div >
@@ -28,8 +51,13 @@ const Register = () => {
             <input type="text" name="lastname*" placeholder="Apellido*"/>
             <input type="text" name="DNI" placeholder="DNI*"/>
             <input type="text" name="email" placeholder="Correo electronico"/>
-            <input type="text" name="password" />
-            <Link href="#">Crear Cuenta</Link>
+            <input {...register("password")} />
+        <p> {errors.password?.message}</p>
+        {error && <p>Contraseña incorrecta. Vuelve a intentarlo</p>}
+        <input type="submit" value="Crear Cuenta" />
+          
+          
+
           </form>
         </div>
       </main>
@@ -37,3 +65,7 @@ const Register = () => {
 
 }
 export default Register
+function setError(arg0: boolean) {
+  throw new Error('Function not implemented.');
+}
+
