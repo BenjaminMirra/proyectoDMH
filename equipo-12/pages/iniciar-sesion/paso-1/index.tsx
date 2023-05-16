ab8f5b81718cef812c5bd641bcc3a3a0ef4014d7
Diana <dianasauval@hotmail.com>
2023-05-16 13:29:38 -0300

    igualando estilos del login y el register

--- equipo-12/pages/iniciar-sesion/paso-1/index.tsx
+++ equipo-12/pages/iniciar-sesion/paso-1/index.tsx
@@ -6,8 +6,9 @@ import { useRouter } from "next/router";
 import Header from "../../../components/Header/header";
 import Footer from "../../../components/Footer/footer";
 import Head from "next/head";
-import { Button, Grid, Input, TextField } from "@mui/material";
-import styles from "../../../styles/login.module.css"
+import { Box, Button, Grid, Input, TextField } from "@mui/material";
+import styles from "../../../styles/login.module.css";
+import ControlledInput from "../../../components/FormController/controlled-input";
 
 const schema = yup
   .object({
@@ -22,44 +23,83 @@ type FormData = yup.InferType<typeof schema>;
 const Username = () => {
   const router = useRouter();
   const {
-    register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isSubmitting },
+    control,
   } = useForm<FormData>({
     resolver: yupResolver(schema),
   });
+
   const onSubmit = (data: FormData) =>
     router.push({
       pathname: "/iniciar-sesion/paso-2",
       query: { email: data.email },
     });
 
-    const onClick = () => {
-      router.push("/");
-    };
-    
+  const onClick = () => {
+    router.push("/");
+  };
+
   return (
     <>
-    <Head>
-      <title>Digital Money House</title>
-      <meta name="iniciar-sesion" content="Digital Money House" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-    </Head>
-    <main>
-      <Header />
-      <Grid className={styles.gridGeneralContainer}>
-      <h2>¡Hola! Ingresá tu e-mail</h2>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <TextField id="outlined-basic"  variant="outlined" className={styles.inputForm} {...register("email")} />
-        <p className={styles.pError}> {errors.email?.message}</p>
-        <Button variant="secondary" type="submit">Continuar</Button>
-      </form>
-      <Button variant="tertiary" onClick={()=>onClick()}>Crear cuenta</Button>
+      <Head>
+        <title>Digital Money House</title>
+        <meta name="iniciar-sesion" content="Digital Money House" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main>
+        <Header />
+        <Grid className={styles.gridGeneralContainer}>
+          <h2>¡Hola! Ingresá tu e-mail</h2>
+          <form onSubmit={handleSubmit(onSubmit)}>
+          <Box
+        sx={{
+          display: "grid",
+          gridTemplateColumns: "repeat(1, 1fr)",
+          gridTemplateRows: "1fr 1fr 0.25fr 1fr 1fr",
+          gridColumnGap: "62px",
+          gridRowGap: "40px",
+          "& button ": {
+            borderRadius: "10px",
+          },
+          "& input": {
+            backgroundColor: "#FFFF",
+            borderRadius: "10px",
+            height: "31px",
+          },
+          "& .css-1av9oub-MuiInputBase-root-MuiFilledInput-root::after, & .css-1av9oub-MuiInputBase-root-MuiFilledInput-root::before":
+            {
+              display: "none",
+            },
+          "& .css-1yuncik-MuiFormLabel-root-MuiInputLabel-root": {
+            transform: "translate(12px, 20px) scale(1)",
+          },
+        }}
+      >
+              <ControlledInput
+                name="email"
+                control={control}
+                type="text"
+                label="Correo electronico*"
+                errorMessage={errors["email"]?.message}
+                variant="filled"
+              />
 
-    </Grid>
-      <Footer />
-    </main>
-  </>    
+              {/*  <TextField id="filled-basic"  variant="filled" className={styles.inputForm} {...register("email")} />
+        <p className={styles.pError}> {errors.email?.message}</p> */}
+              <Button variant="secondary" type="submit">
+                Continuar
+              </Button>
+ 
+          <Button variant="tertiary" onClick={() => onClick()}>
+            Crear cuenta
+          </Button>
+          </Box>
+          </form>
+        </Grid>
+        <Footer />
+      </main>
+    </>
   );
 };
 
--- equipo-12/pages/iniciar-sesion/paso-2/index.tsx
+++ equipo-12/pages/iniciar-sesion/paso-2/index.tsx
@@ -8,8 +8,9 @@ import { useState } from "react";
 import Head from "next/head";
 import Header from "../../../components/Header/header";
 import Footer from "../../../components/Footer/footer";
-import { Button, Grid, TextField } from "@mui/material";
-import styles from "../../../styles/login.module.css"
+import { Box, Button, Grid, TextField, Typography } from "@mui/material";
+import styles from "../../../styles/login.module.css";
+import ControlledInput from "../../../components/FormController/controlled-input";
 
 const schema = yup
   .object({
@@ -19,13 +20,14 @@ const schema = yup
 type FormData = yup.InferType<typeof schema>;
 
 const Password = () => {
-  const [error, setError] = useState(false);
+  const [errorLogin, setErrorLogin] = useState(false);
   const router = useRouter();
   const { query } = useRouter();
   const {
     register,
     handleSubmit,
     formState: { errors },
+    control,
   } = useForm<FormData>({
     resolver: yupResolver(schema),
   });
@@ -39,36 +41,81 @@ const Password = () => {
         })
         .then(function (response) {
           console.log(response);
-          setError(false);
+          setErrorLogin(false);
           router.push("/");
         });
     } catch (error) {
       console.log(error);
-      setError(true);
+      setErrorLogin(true);
     }
   };
 
   return (
     <>
-    <Head>
-      <title>Digital Money House</title>
-      <meta name="iniciar-sesion" content="Digital Money House" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-    </Head>
-    <main>
-      <Header />
-      <Grid className={styles.gridGeneralContainer}>
-      <h2>Ingresá tu contraseña</h2>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <TextField id="outlined-basic"  variant="outlined" className={styles.inputForm} type="password" {...register("password")} />
-        <p> {errors.password?.message}</p>
-        {error && <p>Contraseña incorrecta. Vuelve a intentarlo</p>}
-        <Button variant="secondary" type="submit">Continuar</Button>
-      </form>
-    </Grid>
-      <Footer />
-    </main>
-  </>
+      <Head>
+        <title>Digital Money House</title>
+        <meta name="iniciar-sesion" content="Digital Money House" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main>
+        <Header />
+        <Grid className={styles.gridGeneralContainer}>
+          <h2>Ingresá tu contraseña</h2>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Box
+              sx={{
+                display: "grid",
+                gridTemplateColumns: "repeat(1, 1fr)",
+                gridTemplateRows: "1fr 1fr 0.25fr 1fr 1fr",
+                gridColumnGap: "62px",
+                gridRowGap: "40px",
+                "& button ": {
+                  borderRadius: "10px",
+                },
+                "& input": {
+                  backgroundColor: "#FFFF",
+                  borderRadius: "10px",
+                  height: "31px",
+                },
+                "& .css-1av9oub-MuiInputBase-root-MuiFilledInput-root::after, & .css-1av9oub-MuiInputBase-root-MuiFilledInput-root::before":
+                  {
+                    display: "none",
+                  },
+                "& .css-1yuncik-MuiFormLabel-root-MuiInputLabel-root": {
+                  transform: "translate(12px, 20px) scale(1)",
+                },
+              }}
+            >
+              <ControlledInput
+                name="password"
+                control={control}
+                type="password"
+                label="Contraseña*"
+                errorMessage={errors["password"]?.message}
+                variant="filled"
+              />
+              {errorLogin && (
+                <Typography
+                  sx={{
+                    position: "absolute",
+                    color: "#CC0000",
+                    fontSize: "14px",
+                    fontStyle: "italic",
+                    marginTop:"65px"
+                  }}
+                >
+                  Contraseña incorrecta. Vuelve a intentarlo
+                </Typography>
+              )}
+              <Button variant="secondary" type="submit">
+                Continuar
+              </Button>
+            </Box>
+          </form>
+        </Grid>
+        <Footer />
+      </main>
+    </>
   );
 };
 
