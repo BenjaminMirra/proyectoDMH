c07d21adb8c1e64c2350ef0169ba1cdcb55d7913
MauriBoye <mauriboye@gmail.com>
2023-05-17 12:51:37 -0300

    Merge develop errors

--- equipo-12/components/Header/header-home.tsx
+++ equipo-12/components/Header/header-home.tsx
@@ -6,7 +6,6 @@ import { Box } from "@mui/material";
 
 const HeaderHome = () => {
   return (
-<<<<<<< HEAD
     <Box
       sx={{
         backgroundColor: "var(--main-bg-color)",
@@ -48,57 +47,6 @@ const HeaderHome = () => {
             </Button>
           </Link>
         </Box>
-=======
-    <Box sx={{
-      display: "flex",
-      justifyContent: "space-between",
-      paddingRight: "20px",
-      paddingBottom: "12px",
-      paddingLeft: "20px",
-      paddingTop: "15.5px",
-      backgroundColor: "#181818",
-      "@media (max-width:768px)": {
-        paddingRight: "10px",
-        paddingLeft: "10px",
-      },
-    }}>
-      <Box>
-        <Link href={"/"}>
-          <Image
-            src={imageLogo}
-            alt="logo"
-            width={86}
-            height={40}
-          />
-        </Link>
-      </Box>
-      <Box sx={{
-        display: "flex",
-        gap: "20px",
-      }}>
-        <Link href="/iniciar-sesion/paso-1">
-          <Button
-            variant="primary"
-            size="medium"
-            sx={{
-              width: "99px !important"
-            }}
-          >
-            Ingresar
-          </Button>
-        </Link>
-        <Link href="/crear-cuenta">
-          <Button
-            variant="primary"
-            color="secondary"
-            sx={{
-              width: "131px !important"
-            }}
-          >
-            Crear Cuenta
-          </Button>
-        </Link>
->>>>>>> 1b76dea782487ffef90636f59a30087fb16e3504
       </Box>
     </Box>
   );
