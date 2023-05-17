3f6f10150ccd3e244493b70b243e864462314690
Diana <dianasauval@hotmail.com>
2023-05-16 23:41:41 -0300

    done SuccessRegister page

--- equipo-12/components/Header/header.tsx
+++ equipo-12/components/Header/header.tsx
@@ -1,35 +1,54 @@
 import Image from "next/image";
-import styles from "./header.module.css";
-import imageLogo from "../../utils/images/imageLogo.svg";
-import Button from "@mui/material/Button";
+import logoDark from "../../utils/images/logoDark.svg";
+import { Button, Box } from "@mui/material";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Header = () => {
+  const router = useRouter();
+
   return (
-    <div className={styles.header}>
-      <div>
-        <Image
-          src={imageLogo}
-          alt="logo"
-          width={86}
-          height={33}
-        />
-      </div>
-      <div className={styles.buttonContainer}>
-        <Link href="/iniciar-sesion">
-          <Button variant="primary">
-            Ingresar
-          </Button>
+    <Box
+      sx={{
+        backgroundColor: "#C1FD35",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+          padding: "10px 20px",
+        }}
+      >
+        <Link
+          href="/"
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Image src={logoDark} alt="logo" />
         </Link>
-        <Link href="/crear-cuenta">
-          <Button variant="secondary">
-            Crear Cuenta
-          </Button>
-        </Link>
-      </div>
-
-
-    </div>
+        <Box>
+          {router.pathname === "/iniciar-sesion/paso-1" ||
+          router.pathname === "/iniciar-sesion/paso-2" ? (
+            <></>
+          ) : (
+            <>
+              <Link href="/iniciar-sesion/paso-1">
+                <Button variant="primary">Ingresar</Button>
+              </Link>
+              <Link href="/crear-cuenta">
+                <Button variant="secondary">Crear Cuenta</Button>
+              </Link>
+            </>
+          )}
+        </Box>
+      </Box>
+    </Box>
   );
 };
 
---
+++ equipo-12/pages/registro-exitoso/index.tsx
@@ -0,0 +1,70 @@
+import { Box, Button, Typography } from "@mui/material";
+import Head from "next/head";
+import LayoutRegister from "../../layout/layout-register";
+import FormRegister from "../../components/FormController/form-register";
+import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
+import Link from "next/link";
+
+const SuccessRegister = () => {
+  return (
+    <LayoutRegister>
+      <Head>
+        <title>Registro DMH</title>
+        <meta name="description" content="Registro Digital Money House" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="" />
+      </Head>
+      <main
+        style={{
+          margin: "0 auto",
+          flex: 1,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          width: "100%",
+          backgroundColor: "#201F22",
+          padding: "25px",
+        }}
+      >
+        <Typography
+          variant="h1"
+          sx={{
+            marginBottom: "40px",
+            color: "#FFFF",
+          }}
+        >
+          Registro Exitoso
+        </Typography>
+        <Box
+          sx={{
+            width: "35%",
+            textAlign: "center",
+          }}
+        >
+          <CheckCircleOutlineIcon
+            sx={{
+              color: "#C1FD35",
+              fontSize: "70px",
+              marginBottom: "30px",
+            }}
+          />
+          <Typography
+            variant="subtitle2"
+            sx={{
+              marginBottom: "40px",
+              color: "#FFFF",
+            }}
+          >
+            Hemos enviado un correo de confirmación para validar tu email, por
+            favor revisalo para iniciar sesión
+          </Typography>
+        </Box>
+        <Link href="/">
+          <Button variant="secondary">Continuar</Button>
+        </Link>
+      </main>
+    </LayoutRegister>
+  );
+};
+export default SuccessRegister;
