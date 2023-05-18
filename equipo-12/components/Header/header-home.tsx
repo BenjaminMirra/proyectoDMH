e86985f745e978ae1627da8f268d1853158135cd
Diana <dianasauval@hotmail.com>
2023-05-18 18:57:33 -0300

    agragada funcionalidad del logout

--- equipo-12/components/Header/header-home.tsx
+++ equipo-12/components/Header/header-home.tsx
@@ -2,35 +2,38 @@ import Image from "next/image";
 import imageLogo from "../../utils/images/imageLogo.svg";
 import Button from "@mui/material/Button";
 import Link from "next/link";
-import { Box, Typography } from "@mui/material";
+import { Box, Fade, Menu, MenuItem, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import TemporaryDrawer from "../Drawer/Drawer";
 
 const HeaderHome = () => {
-
+  const [logged, setLogged] = useState(false);
   const [userData, setUserData] = useState({
     name: "",
-    lastName: ""
+    lastName: "",
   });
 
   useEffect(() => {
     if (localStorage.getItem("userId") !== null) {
       const userId = localStorage.getItem("userId");
-      const token = localStorage.getItem("token");
+      const token = localStorage.getItem("token");      
       const config = {
         method: "get",
         url: `https://digitalmoney.ctd.academy/api/users/${userId}`,
         headers: {
-          "Authorization": `${token}`
+          Authorization: `${token}`,
         },
-        data: ""
+        data: "",
       };
 
-      axios.request(config)
+      axios
+        .request(config)
         .then((response) => {
+          setLogged(true);
           setUserData({
             name: response.data.firstname,
-            lastName: response.data.lastname
+            lastName: response.data.lastname,
           });
         })
         .catch((error) => {
@@ -38,6 +41,111 @@ const HeaderHome = () => {
         });
     }
   }, []);
+
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleLogout = () => {
+    setAnchorEl(null);
+    localStorage.removeItem("token");
+    setLogged(false);
+  };
+
+  const handleIsAuthMenu = () => {
+    if (logged) {
+      ////const { nombre, apellido } = JSON.parse(localStorage.getItem('user'));
+      return (
+        <>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              backgroundColor: "#C1FD35",
+              color: "var( --main-bg-color)",
+              padding: "5px",
+              borderRadius: "10px",
+            }}
+          >
+            <Button
+              id="fade-button"
+              aria-controls={open ? "fade-menu" : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? "true" : undefined}
+              onClick={handleClick}      
+            >
+              <Typography
+                variant="h4"
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  lineHeight: "24px",
+                  color: "var( --main-bg-color)",
+                }}
+              >
+                {userData.name.charAt(0)}
+                {userData.lastName.charAt(0)}
+              </Typography>
+            </Button>
+            <Menu
+              id="fade-menu"
+              MenuListProps={{
+                "aria-labelledby": "fade-button",
+              }}
+              anchorEl={anchorEl}
+              open={open}
+              TransitionComponent={Fade}
+              onClick={handleClose}
+              sx={{
+                margin:"5px"
+              }}
+            >
+              <MenuItem onClick={handleLogout}>Logout</MenuItem>
+            </Menu>
+          </Box>
+
+          <Typography
+            variant="subtitle2"
+            sx={{
+              "@media (max-width: 768px)": {
+                display: "none",
+              },
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              fontWeight: "700",
+              fontSize: "16px",
+              color: "var(--main-text-color)",
+            }}
+          >
+            Hola, {userData.name} {userData.lastName}
+          </Typography>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <Link href="/iniciar-sesion/paso-1">
+            <Button variant="primary" size="small">
+              Ingresar
+            </Button>
+          </Link>
+          <Link href="/registro">
+            <Button variant="primary" color="secondary" size="small">
+              Crear Cuenta
+            </Button>
+          </Link>
+        </>
+      );
+    }
+  };
 
   return (
     <Box
@@ -70,65 +178,13 @@ const HeaderHome = () => {
           sx={{
             display: "flex",
             gap: "20px",
+            color:"white"
           }}
         >
-          {userData.name === "" ?
-            <>
-              <Link href="/iniciar-sesion/paso-1">
-                <Button variant="primary" size="small">
-                  Ingresar
-                </Button>
-              </Link>
-              <Link href="/registro">
-                <Button variant="primary" color="secondary" size="small">
-                  Crear Cuenta
-                </Button>
-              </Link>
-            </>
-            :
-            <>
-              <Box
-                sx={{
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                  backgroundColor: "#C1FD35",
-                  color: "var( --main-bg-color)",
-                  padding: "5px",
-                  borderRadius: "10px",
-                }}>
-                <Typography
-                  variant="h4"
-                  sx={{
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    lineHeight: "24px",
-                    color: "var( --main-bg-color)",
-                  }}>
-                  {userData.name.charAt(0)}{userData.lastName.charAt(0)}
-                </Typography>
-              </Box>
-              <Typography
-                variant="subtitle2"
-                sx={{
-                  "@media (max-width: 768px)": {
-                    display: "none",
-                  },
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                  fontWeight: "700",
-                  fontSize: "16px",
-                  color: "var(--main-text-color)",
-                }}
-              >Hola, {userData.name} {userData.lastName}
-              </Typography>
-            </>
-          }
+          {handleIsAuthMenu()}          
         </Box>
       </Box>
-    </Box >
+    </Box>
   );
 };
 
