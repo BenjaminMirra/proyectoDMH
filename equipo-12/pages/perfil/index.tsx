d4624af72ef91d69ffaf770f0216db8bbf223e9c
Benjamin Mirra <mirrabenjamin@gmail.com>
2023-05-24 19:03:50 -0300

    creating branch

---
+++ equipo-12/pages/perfil/index.tsx
@@ -0,0 +1,14 @@
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+
+const Perfil = () => {
+  return (
+    <Box>
+      <Typography>
+        TuPerfil
+      </Typography>
+    </Box>
+  );
+};
+
+export default Perfil;
