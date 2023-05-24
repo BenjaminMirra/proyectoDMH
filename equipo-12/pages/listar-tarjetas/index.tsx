7f221c974e992f2f2a83518b406bad8758c0d6a8
Maite Lopez <mayte_mdp@hotmail.com>
2023-05-23 23:24:56 -0300

    Creating page with list of cards

---
+++ equipo-12/pages/listar-tarjetas/index.tsx
@@ -0,0 +1,14 @@
+import { Box, Button, Typography } from "@mui/material";
+import Head from "next/head";
+import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
+import Link from "next/link";
+import LayoutLogin from "../../layout/layout-login";
+
+const ListCards = () => {
+  return (
+    <div>
+        Breanch ListCards Create
+    </div>
+  );
+};
+export default ListCards;
