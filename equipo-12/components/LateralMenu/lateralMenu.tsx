2fb520efe8e87e5ffeec7feb626722240c5bdc00
Benjamin Mirra <mirrabenjamin@gmail.com>
2023-05-30 21:21:54 -0300

    adding to pull develop

--- equipo-12/components/LateralMenu/lateralMenu.tsx
+++ equipo-12/components/LateralMenu/lateralMenu.tsx
@@ -4,67 +4,67 @@ import Box from "@mui/material/Box";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { Typography } from "@mui/material";
-import close from "../../utils/images/close.svg"
+import close from "../../utils/images/close.svg";
 import Image from "next/image";
 
 
-const LateralMenu = ( props : any) => {
-    const { visibility, setVisibility } = props;
-    const [userData, setUserData] = useState({
-        name: "",
-        lastName: "",
-    });
-    useEffect(() => {
-        if (localStorage.getItem("userId") !== null) {
-        const userId = localStorage.getItem("userId");
-        const token = localStorage.getItem("token");
-        const config = {
-            method: "get",
-            url: `https://digitalmoney.ctd.academy/api/users/${userId}`,
-            headers: {
-                Authorization: `${token}`,
-            },
-            data: "",
-        };
-        axios
-            .request(config)
-            .then((response) => {
-            setUserData({
-                name: response.data.firstname,
-                lastName: response.data.lastname,
-            });
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-        }
-    }, []);
+const LateralMenu = (props: any) => {
+  const { visibility, setVisibility } = props;
+  const [userData, setUserData] = useState({
+    name: "",
+    lastName: "",
+  });
+  useEffect(() => {
+    if (localStorage.getItem("userId") !== null) {
+      const userId = localStorage.getItem("userId");
+      const token = localStorage.getItem("token");
+      const config = {
+        method: "get",
+        url: `https://digitalmoney.ctd.academy/api/users/${userId}`,
+        headers: {
+          Authorization: `${token}`,
+        },
+        data: "",
+      };
+      axios
+        .request(config)
+        .then((response) => {
+          setUserData({
+            name: response.data.firstname,
+            lastName: response.data.lastname,
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }, []);
 
-    return(
-        <>
-            <Box 
-                className={`${styles.mobileBackground} ${visibility ? styles.visible : styles.invisible}`} 
-                onClick={() => {setVisibility(false)}}
-            >
-                <Box className={`${styles.top} ${visibility ? styles.animationFoward : styles.animationBack}`}>
-                        <Box>
-                            <button onClick={()=>{setVisibility(false)}}><Image src={close} alt="logo"/></button>
-                        </Box>
-                        <Typography>Hola,</Typography>
-                        <Typography>{userData.name}  {userData.lastName}</Typography>
-                </Box>
-                <Box className={`${styles.container} ${styles.mobileContainer} ${visibility ? styles.animationFoward : styles.animationBack}`}>
-                    <NavTree/>
-                </Box>
-            </Box>
-            <Box className={styles.desktopBackground}>
-                    <Box className={styles.container}>
-                        <NavTree/>
-                    </Box>
-            </Box>
-        </>
-    )
-}
+  return (
+    <>
+      <Box
+        className={`${styles.mobileBackground} ${visibility ? styles.visible : styles.invisible}`}
+        onClick={() => { setVisibility(false); }}
+      >
+        <Box className={`${styles.top} ${visibility ? styles.animationFoward : styles.animationBack}`}>
+          <Box>
+            <button onClick={() => { setVisibility(false); }}><Image src={close} alt="logo" /></button>
+          </Box>
+          <Typography>Hola,</Typography>
+          <Typography>{userData.name}  {userData.lastName}</Typography>
+        </Box>
+        <Box className={`${styles.container} ${styles.mobileContainer} ${visibility ? styles.animationFoward : styles.animationBack}`}>
+          <NavTree />
+        </Box>
+      </Box>
+      <Box className={styles.desktopBackground}>
+        <Box className={styles.container}>
+          <NavTree />
+        </Box>
+      </Box>
+    </>
+  );
+};
 
 export default LateralMenu;
 
