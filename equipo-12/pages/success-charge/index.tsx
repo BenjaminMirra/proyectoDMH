cb5c5fac61714dc96a0e707c53ea5ec196619a90
Benjamin Mirra <mirrabenjamin@gmail.com>
2023-06-08 21:22:24 -0300

    hola

--- equipo-12/pages/success-charge/index.tsx
+++ equipo-12/pages/success-charge/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import { ReactElement } from "react";
 import Layout from "../../layout/layout";
 import Head from "next/head";
