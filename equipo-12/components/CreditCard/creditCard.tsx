c28808e5d1982a898b3b01d15d371fcb3d335db4
Maite Lopez <mayte_mdp@hotmail.com>
2023-06-13 00:30:54 -0300

    [FIX]Redireccionar a distintas page desde agregar-tarjeta

--- equipo-12/components/CreditCard/creditCard.tsx
+++ equipo-12/components/CreditCard/creditCard.tsx
@@ -83,6 +83,13 @@ const CreditCard = ({listar}: PropsCard) => {
       }
     }
   };
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  }));
 
 
   return (
