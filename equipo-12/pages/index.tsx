632a7c45ae3e58ceac3240fc9ed4f5598439a50e
Benjamin Mirra <mirrabenjamin@gmail.com>
2023-06-22 22:27:35 -0300

    [fix] deploy

--- equipo-12/pages/index.tsx
+++ equipo-12/pages/index.tsx
@@ -1,15 +1,13 @@
 import Head from "next/head";
-import clientPromise from "../lib/mongodb";
 import { Hero } from "../components/Hero/hero";
 import { ReactElement } from "react";
 import { NextPageWithLayout } from "./_app";
 import Layout from "../layout/layout";
-import { GetServerSideProps } from "next";
 interface PropsType {
   children?: ReactElement;
 }
 
-const Home: NextPageWithLayout<PropsType> = ({ texts, images, cards }: any) => {
+const Home: NextPageWithLayout<PropsType> = () => {
 
   return (
     <>
@@ -32,7 +30,7 @@ const Home: NextPageWithLayout<PropsType> = ({ texts, images, cards }: any) => {
           backgroundColor: "#201F22",
         }}
       >
-        <Hero texts={texts} images={images} cards={cards} />
+        <Hero />
       </main>
     </>
   );
@@ -40,62 +38,6 @@ const Home: NextPageWithLayout<PropsType> = ({ texts, images, cards }: any) => {
 
 Home.getLayout = function getLayout(page: ReactElement) {
   return <Layout variant="home">{page}</Layout>;
-};
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  try {
-    const client = await clientPromise;
-    const db = client.db("data");
-
-    const texts = await db
-      .collection("texts")
-      .find({})
-      .sort({ metacritic: -1 })
-      .limit(2)
-      .toArray();
-
-    const transformedTexts = texts.map((text) => {
-      const titles = text.titles.map((title: any) => title.title);
-      return { titles };
-    });
-
-    const cards = await db
-      .collection("cards")
-      .find({})
-      .sort({ metacritic: -1 })
-      .limit(2)
-      .toArray();
-
-    const transformedCards = cards.map((card) => {
-      const transformedData = {
-        titles: card.cards.map((item: any) => ({
-          title: item.title,
-          description: item.description,
-        })),
-      };
-      return transformedData;
-    });
-
-    const images = await db
-      .collection("images")
-      .find({})
-      .sort({ metacritic: -1 })
-      .limit(2)
-      .toArray();
-
-    return {
-      props: {
-        texts: transformedTexts,
-        images: JSON.parse(JSON.stringify(images)),
-        cards: transformedCards,
-      },
-    };
-  } catch (e) {
-    console.error(e);
-    return {
-      props: { ok: false, reason: "No se puede acceder a la Base de Datos" },
-    };
-  }
 };
 
 export default Home;
