da361b782cd3daf8093410ccc01976f846af9a7f
Julian Paris <julian.pariss@hotmail.com>
2023-06-26 23:41:20 -0300

    ingresar numero de cuenta

--- equipo-12/components/Services/itemService.tsx
+++ equipo-12/components/Services/itemService.tsx
@@ -1,26 +1,43 @@
-import { Avatar, Button, Divider, ListItem, ListItemAvatar, ListItemText } from "@mui/material";
+import {
+  Avatar,
+  Button,
+  Divider,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+} from "@mui/material";
 import Link from "next/link";
 import { IService } from "../../types";
 
 type Props = {
   service: IService;
-
-}
+};
 
 const ItemService = ({ service }: Props) => {
   return (
-    <Link href={`/?/${service.id}`}
-      style={{ textDecoration: "none", color: "unset" }}>
-      <ListItem alignItems="flex-start"
-        sx={{ display: "flex", alignItems: "center", "&:hover": { backgroundColor: "#EEEAEA", }, }}>
+    <Link
+      href={`/ingresar-numero-cuenta/${service.id}`}
+      style={{ textDecoration: "none", color: "unset" }}
+    >
+      <ListItem
+        alignItems="flex-start"
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          "&:hover": { backgroundColor: "#EEEAEA" },
+        }}
+      >
         <ListItemAvatar>
           <Avatar alt="R" sx={{ backgroundColor: "#C1FD35", color: "#000" }} />
         </ListItemAvatar>
         <ListItemText
-          sx={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
           primary={service.name}
-        >
-        </ListItemText>
+        ></ListItemText>
         <Button>Seleccionar</Button>
       </ListItem>
     </Link>
