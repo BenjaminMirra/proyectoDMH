1db3a866df85cb8a0f384a92a1fd24862944b8fd
Benjamin Mirra <mirrabenjamin@gmail.com>
2023-06-29 22:28:47 -0300

    [FIX] proteceted route inicio

--- equipo-12/pages/inicio/index.tsx
+++ equipo-12/pages/inicio/index.tsx
@@ -1,5 +1,5 @@
-import { Box, Button } from "@mui/material";
-import { ReactElement, useEffect } from "react";
+import { Box, Button, CircularProgress } from "@mui/material";
+import { ReactElement, useEffect, useState } from "react";
 import Layout from "../../layout/layout";
 import AvailableAmount from "../../components/AvailableAmount";
 import Transferences from "../../components/Transferences";
@@ -10,13 +10,20 @@ import { useRouter } from "next/router";
 
 const Inicio = () => {
 
+  const [hasToken, setHasToken] = useState(false);
   const router = useRouter();
 
   useEffect(() => {
     if (!localStorage.getItem("token")) {
-      router.push("/");
+      router.push("/iniciar-sesion/paso-1");
+      setTimeout(() => {
+        setHasToken(true);
+      }, 3000);
+    } else {
+      setHasToken(true);
     }
-  });
+
+  }, [hasToken]);
 
   return (
     <>
@@ -24,34 +31,55 @@ const Inicio = () => {
         <title>Digital Money House</title>
         <meta name="description" content="Digital Money House" />
       </Head>
-      <Box
-        sx={{
-          width: "276px",
-          height: "100%",
-          backgroundColor: "#C1FD35",
-          "@media (max-width: 768px)": {
-            display: "none",
-          },
-          "@media (max-width: 1301px)": {
-            width: "221px"
-          },
-        }}
-      ></Box>
-      <Box sx={{ display: "flex", width: "100%", backgroundColor: "#EEEAEA" }}>
-        <Box sx={{ width: "100%", gap: "10px", display: "flex", flexDirection: "column", alignItems: "center", padding: "50px", paddingLeft: "100px", "@media (max-width: 768px)": { padding: "10px", paddingTop: "50px" } }}>
-          <ArrowSubtitleMobile title="Inicio"></ArrowSubtitleMobile>
-          <AvailableAmount></AvailableAmount>
-          <Box sx={{ display: "flex", width: "100%", gap: "10px", "@media (max-width: 768px)": { flexDirection: "column" } }}>
-            <Link style={{ width: "100%" }} href="/cargar-dinero">
-              <Button sx={{ width: "100%", "@media (max-width: 768px)": { width: "100%" } }} variant="xxl">Cargar Dinero</Button>
-            </Link>
-            <Link style={{ width: "100%" }} href="/listar-servicios">
-              <Button sx={{ width: "100%", "@media (max-width: 768px)": { width: "100%" } }} variant="xxl">Pago de Servicios</Button>
-            </Link>
+      {hasToken ?
+        <>
+          <Box
+            sx={{
+              width: "276px",
+              height: "100%",
+              backgroundColor: "#C1FD35",
+              "@media (max-width: 768px)": {
+                display: "none",
+              },
+              "@media (max-width: 1301px)": {
+                width: "221px"
+              },
+            }}
+          ></Box>
+          <Box sx={{ display: "flex", width: "100%", backgroundColor: "#EEEAEA" }}>
+            <Box sx={{ width: "100%", gap: "10px", display: "flex", flexDirection: "column", alignItems: "center", padding: "50px", paddingLeft: "100px", "@media (max-width: 768px)": { padding: "10px", paddingTop: "50px" } }}>
+              <ArrowSubtitleMobile title="Inicio"></ArrowSubtitleMobile>
+              <AvailableAmount></AvailableAmount>
+              <Box sx={{ display: "flex", width: "100%", gap: "10px", "@media (max-width: 768px)": { flexDirection: "column" } }}>
+                <Link style={{ width: "100%" }} href="/cargar-dinero">
+                  <Button sx={{ width: "100%", "@media (max-width: 768px)": { width: "100%" } }} variant="xxl">Cargar Dinero</Button>
+                </Link>
+                <Link style={{ width: "100%" }} href="/listar-servicios">
+                  <Button sx={{ width: "100%", "@media (max-width: 768px)": { width: "100%" } }} variant="xxl">Pago de Servicios</Button>
+                </Link>
+              </Box>
+              <Transferences></Transferences>
+            </Box>
           </Box>
-          <Transferences></Transferences>
+        </>
+        :
+        <Box
+          sx={{
+            width: "100vh",
+            height: "100vh",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <CircularProgress
+            sx={{
+              color: "var(--lime-green)",
+            }}
+          />
         </Box>
-      </Box>
+      }
     </>
   );
 };
